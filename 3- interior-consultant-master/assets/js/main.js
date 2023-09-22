6a328e091460c35e69570f9e332b551ab3753030
Marcos <marcoswodbarros@outlook.com>
2023-09-22 15:00:32 -0300

    Mobile: Refactoring main display

--- 3- interior-consultant-master/assets/js/main.js
+++ 3- interior-consultant-master/assets/js/main.js
@@ -2,18 +2,19 @@ function showMenu () {
     let openMenu = document.querySelector('.fa-bars');
     let closeMenu = document.querySelector('.fa-xmark');
     let ul = document.querySelector('ul');
+    let main = document.querySelector('main');
+    let footer = document.querySelector('footer');
 
     if (ul.classList.contains('nav__ul--expanded')) {
         ul.classList.remove('nav__ul--expanded');
+        main.classList.remove('main__menu--expanded');
         openMenu.style.display = 'block';
         closeMenu.style.display = 'none';
         
     } else {
         ul.classList.add('nav__ul--expanded');
+        main.classList.add('main__menu--expanded');
         openMenu.style.display = 'none';
-        closeMenu.style.display = 'block';
-
+        closeMenu.style.display = 'block';    
     }
 }
-
-
