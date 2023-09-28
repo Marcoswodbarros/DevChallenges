9e0931fecc7c6c57c9c2f516f6afa8e521189961
Marcos <marcoswodbarros@outlook.com>
2023-09-28 12:58:40 -0300

    Creating a semantic js

--- 3- interior-consultant-master/assets/js/main.js
+++ 3- interior-consultant-master/assets/js/main.js
@@ -1,46 +1,30 @@
-let bars = document.querySelector('.fa-bars');
-let x = document.querySelector('.fa-xmark');
-let list = document.querySelector('ul');
+const bars = document.querySelector('.fa-bars');
+const x = document.querySelector('.fa-xmark');
+const list = document.querySelector('ul');
 
-bars.addEventListener('click', function () {
+function openMenu() {
     list.classList.add('nav__ul--expanded');
-    bars.style.display = 'none';
-    x.style.display = 'block';
-});
-
-x.addEventListener('click', function () {
-    list.classList.remove('nav__ul--expanded');
-    bars.style.display = 'block';
-    x.style.display = 'none';
-});
-
-// Função para atualizar a visibilidade dos ícones com base na largura da tela.
-function updateIconVisibility() {
-    if (window.innerWidth >= 1024) {
-        // Se a largura da tela for maior ou igual a 1024px, oculte ambos os ícones.
-        bars.style.display = 'none';
-        x.style.display = 'none';
-
-        // Verifique se o menu está expandido e, se estiver, feche-o.
-        if (list.classList.contains('nav__ul--expanded')) {
-            list.classList.remove('nav__ul--expanded');
-        }
-    } else {
-        // Caso contrário, mostre o ícone "bars" se o menu não estiver expandido,
-        // caso contrário, mostre o ícone "x".
-        if (!list.classList.contains('nav__ul--expanded')) {
-            bars.style.display = 'block';
-            x.style.display = 'none';
-        } else {
-            bars.style.display = 'none';
-            x.style.display = 'block';
-        }
-    }
+    updateIconVisibility();
 }
 
-// Adicione um ouvinte de evento de redimensionamento.
+function closeMenu() {
+    list.classList.remove('nav__ul--expanded');
+    updateIconVisibility();
+}
+
+function updateIconVisibility() {
+    if (window.innerWidth >= 1024 && list.classList.contains('nav__ul--expanded')) {
+        closeMenu();
+    }
+
+    bars.style.display = window.innerWidth < 1024 && !list.classList.contains('nav__ul--expanded') ? 'block' : 'none';
+    x.style.display = window.innerWidth < 1024 && list.classList.contains('nav__ul--expanded') ? 'block' : 'none';
+}
+
+bars.addEventListener('click', openMenu);
+x.addEventListener('click', closeMenu);
 window.addEventListener('resize', updateIconVisibility);
 
-// Chame a função inicialmente para definir a visibilidade correta dos ícones.
+// Chame a função inicialmente para definir a visibilidade correta dos ícones
 updateIconVisibility();
 
