6f661ec60381af071d8f64a47424f8fbc3f77c46
Marcos Barros <marcoswodbarros@outlook.com>
2023-10-20 11:05:13 -0300

    JS: Finishing first total

--- 6- checkout-page-master/assets/js/main.js
+++ 6- checkout-page-master/assets/js/main.js
@@ -2,8 +2,10 @@
 const firstDecrement = document.querySelector('.firstDecrement');
 const firstIncrement = document.querySelector('.firstIncrement');
 const firstQuantity = document.querySelector('.firstQuantity');
+const total = document.querySelector('.totalValue');
 
 let quantity = 1;
+let totalQuantity = 148.98;
 
 
 // Events
@@ -11,16 +13,22 @@ firstDecrement.addEventListener('click', () => {
     if (quantity > 1) {
         quantity--;
         updateFirstQuantity();
+        updateTotal();
     }
 });
 
 firstIncrement.addEventListener('click', () => {
     quantity++;
     updateFirstQuantity();
+    updateTotal();
 });
 
 
 // Functions
 function updateFirstQuantity() {
     firstQuantity.textContent = quantity;
+}
+
+function updateTotal() {
+    total.textContent = `$ ${quantity * 54.99}`;
 }
