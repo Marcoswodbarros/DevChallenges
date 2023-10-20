b858d5a1c2749dfc518a3f21d568bc2ed406baab
Marcos Barros <marcoswodbarros@outlook.com>
2023-10-20 12:51:04 -0300

    JS: Finishing car

--- 6- checkout-page-master/assets/js/main.js
+++ 6- checkout-page-master/assets/js/main.js
@@ -2,33 +2,59 @@
 const firstDecrement = document.querySelector('.firstDecrement');
 const firstIncrement = document.querySelector('.firstIncrement');
 const firstQuantity = document.querySelector('.firstQuantity');
+
+const secondDecrement = document.querySelector('.secondDecrement');
+const secondIncrement = document.querySelector('.secondIncrement');
+const secondQuantity = document.querySelector('.secondQuantity');
+
 const total = document.querySelector('.totalValue');
 
-let quantity = 1;
+let firstQuantityValue = 1;
+let secondQuantityValue = 1;
 let totalQuantity = 148.98;
 
 
 // Events
 firstDecrement.addEventListener('click', () => {
-    if (quantity > 1) {
-        quantity--;
+    if (firstQuantityValue > 1) {
+        firstQuantityValue--;
         updateFirstQuantity();
         updateTotal();
     }
 });
 
 firstIncrement.addEventListener('click', () => {
-    quantity++;
+    firstQuantityValue++;
     updateFirstQuantity();
+    updateTotal();
+});
+
+
+secondDecrement.addEventListener('click', () => {
+    if (secondQuantityValue > 1) {
+        secondQuantityValue--;
+        updateSecondQuantity();
+        updateTotal();
+    }
+});
+
+secondIncrement.addEventListener('click', () => {
+    secondQuantityValue++;
+    updateSecondQuantity();
     updateTotal();
 });
 
 
 // Functions
 function updateFirstQuantity() {
-    firstQuantity.textContent = quantity;
+    firstQuantity.textContent = firstQuantityValue;
+}
+
+function updateSecondQuantity() {
+    secondQuantity.textContent = secondQuantityValue;
 }
 
 function updateTotal() {
-    total.textContent = `$ ${quantity * 54.99}`;
+    const totalPrice = (firstQuantityValue * 54.99) + (secondQuantityValue * 74.99);
+    total.textContent = `$ ${totalPrice.toFixed(2)}`;
 }
