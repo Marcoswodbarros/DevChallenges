e4241308fa7e21377b5a615986dc55fd06b4648d
Marcos Barros <marcoswodbarros@outlook.com>
2023-11-09 10:59:10 -0300

    Mobile: Updating menu - nav style

--- 7- edie-homepage-master/assets/js/main.js
+++ 7- edie-homepage-master/assets/js/main.js
@@ -1,12 +1,23 @@
+// Variables
 const bar = document.querySelector('.fa-bars');
 const xmark = document.querySelector('.fa-xmark');
+const nav = document.querySelector('nav');
 
-bar.addEventListener('click', () => {
-    bar.style.display = 'none';
-    xmark.style.display = 'block';
-});
 
-xmark.addEventListener('click', () => {
-    xmark.style.display = 'none';
-    bar.style.display = 'block';
-})
+
+
+
+// Functions
+function showMenu() {
+    nav.classList.add('ul__expanded');
+
+}
+
+function hideMenu() {
+    nav.classList.remove('ul__expanded');
+
+}
+
+// Events
+bar.addEventListener('click', showMenu);
+xmark.addEventListener('click', hideMenu);
