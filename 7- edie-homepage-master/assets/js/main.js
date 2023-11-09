7b4226e90091c3450b81b882c156923c7c3b7f75
Marcos Barros <marcoswodbarros@outlook.com>
2023-11-09 11:06:29 -0300

    Mobile: Finishing menu (first step)

--- 7- edie-homepage-master/assets/js/main.js
+++ 7- edie-homepage-master/assets/js/main.js
@@ -1,22 +1,22 @@
 // Variables
 const bar = document.querySelector('.fa-bars');
 const xmark = document.querySelector('.fa-xmark');
-const nav = document.querySelector('nav');
-
-
-
+const nav = document.querySelector('ul');
 
 
 // Functions
 function showMenu() {
     nav.classList.add('ul__expanded');
-
+    bar.style.display = 'none';
+    xmark.style.display = 'block';
 }
 
 function hideMenu() {
     nav.classList.remove('ul__expanded');
-
+    xmark.style.display = 'none';
+    bar.style.display = 'block';
 }
+
 
 // Events
 bar.addEventListener('click', showMenu);
